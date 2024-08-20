15a1fad54f3c5a2c913f6a31e904e5ebcf58f591
filipeavila7 <filipeavila076@gmail.com>
2024-08-20 11:53:02 -0300

    atividade ds

---
+++ atividade/atv1.py
@@ -0,0 +1,14 @@
+nm = input('digite seu nome: ')
+idade = int(input('digite sua idade:')) 
+peso = float(input('digite seu peso:'))
+altura = float(input('digite sua altura:'))
+
+print(type(nm))
+print(type(idade))
+print(type(peso))
+print(type(altura))
+
+print(f'o seu nome é: {nm}')
+print(f'o seu nome é: {idade}')
+print(f'o seu nome é: {peso}')
+print(f'o seu nome é: {altura}')
---
+++ atividade/atv2.py
@@ -0,0 +1,4 @@
+num1 = 12
+num2 = 13
+print(f'a soma é: {num1 + num2} ')
+
